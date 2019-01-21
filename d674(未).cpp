2b4a8bd03eb3e45c5f5376fe8f4fb4059951d2e1
kyle355469 <kyle355469@gmail.com>
2019-01-21 23:17:22 +0800

    Create d674(未).cpp
    # 就結束還未做

---
+++ d674(未).cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	unsigned i, l;
+	static int c[101][101] = {};
+	string x, y;
+	int k = 1;
+	while(getline(cin, x)){
+		
+		getline(cin, y);
+		for(i = 1; i <= x.size(); i++){
+			for(l = 1; l <= y.size(); l++){
+				if(x[i - 1] == y[l - 1]){
+					c[i][l] = c[i - 1][l - 1] + 1;
+				}else{
+					c[i][l] = max(c[i][l - 1], c[i - 1][l]);
+				}
+			}
+		}
+		cout << "Case #" << k <<  ": you can visit at most " << c[x.size()][y.size()] << " cities." << endl;
+		k++;
+	}	
+	return 0;
+} 
