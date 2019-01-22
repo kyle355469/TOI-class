cae6c23ee11148ec95955d71a6c137537874af7e
kyle355469 <kyle355469@gmail.com>
2019-01-22 21:17:32 +0800

    Update and rename a133.cpp(錯) to a133.cpp

--- a133.cpp
+++ a133.cpp
@@ -5,8 +5,7 @@ using namespace std;
 int main(){
 	int n1, n2;
 	int k = 1;
-	while(cin >> n1){
-	    cin >> n2;
+	while(cin >> n1 >> n2){
 	    if(n1 == 0 && n2 == 0){
 	    	break;
 		}
@@ -18,12 +17,12 @@ int main(){
 	    for(int i = 0; i < n2; i++){
 	    	cin >> b[i];
 	    }
-	    for(int i = 0; i <= n1; i++){
-	    	for(int j = 0; j <= n2; j++){
-	        	if(a[i] == b[j]){
-	          		height[i + 1][j + 1] = height[i][j] + 1;
+	    for(int i = 1; i <= n1; i++){
+	    	for(int j = 1; j <= n2; j++){
+	        	if(a[i - 1] == b[j - 1]){
+	          		height[i][j] = height[i - 1][j - 1] + 1;
 	        	}else{
-	        		height[i + 1][j + 1] = max(height[i][j + 1], height[i + 1][j]);
+	        		height[i][j] = max(height[i][j - 1], height[i - 1][j]);
 	      		}
 	    	}
 	  	}
