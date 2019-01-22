279d2247e424be5eb543622511d9a21f25184cdc
kyle355469 <kyle355469@gmail.com>
2019-01-22 22:14:11 +0800

    Create b184.cpp

---
+++ b184.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	int n;
+	while(cin >> n){
+		int dp[101] = {};
+		fill(dp + 1, dp + 101, -2e5);
+		while(n > 0){
+			int a, b;
+			cin >> a >> b;
+			for(int i = 100; i >= a; i--){
+				dp[i] = max(dp[i - a] + b, dp[i]);
+			}
+			n--;
+		}
+		cout << *max_element(dp, dp + 101) << endl;
+	}
+	return 0;
+} 
